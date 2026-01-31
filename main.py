e76a048f42cc14172d9650cc2b8241c380f349bf
sarvesh7785 <83919011+sarvesh7785@users.noreply.github.com>
2026-01-31 15:27:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="My First Streamlit App")
 
-st.title("👋 Hello Streamlit")
+st.title("👋 Hello Amrita")
 st.write("This is a simple Streamlit app")
 
 name = st.text_input("Enter your name")
@@ -11,4 +11,5 @@ if name:
     st.success(f"Welcome, {name}!")
 
 number = st.slider("Pick a number", 0, 100, 50)
+
 st.write("You selected:", number)
